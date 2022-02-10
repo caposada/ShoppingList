d7def49fd8eabd290ad7b45caf8eb8f26d9779ea
chris.posada@googlemail.com <chris.posada@googlemail.com>
2022-02-10 15:48:36 +0000

    Added a few tests to teh basketSlice (redux) code

--- ReactClient/src/store/basketSlice.test.ts
+++ ReactClient/src/store/basketSlice.test.ts
@@ -8,6 +8,42 @@ import {
 } from './basketSlice'
 import BasketItem from "@/shared/BasketItem";
 
+function getFirst() {
+    let basketItem: BasketItem = {
+        uniqueCode: "0001",
+        name: "test1",
+        count: 1
+    };
+    return basketItem;
+}
+
+function getSecond() {
+    let basketItem: BasketItem = {
+        uniqueCode: "0002",
+        name: "test2",
+        count: 1
+    };
+    return basketItem;
+}
+
+function getEmpty() {
+    let basketItems: Array<BasketItem> = [];
+    return basketItems;
+}
+
+function getOne() {
+    let basketItems: Array<BasketItem> = [];
+    basketItems.push(getFirst());
+    return basketItems;
+}
+
+function getTwo() {
+    let basketItems: Array<BasketItem> = [];
+    basketItems.push(getFirst());
+    basketItems.push(getSecond());
+    return basketItems;
+}
+
 describe('basketSlice tests', () => {
 
     let state = store.getState().basket;
@@ -25,37 +61,37 @@ describe('basketSlice tests', () => {
     });
 
     describe('test setBasketItems action', () => {        
-        let basketItems: Array<BasketItem> = [];
+        let newBasketItems: Array<BasketItem> = [];
 
         test('should return an empty array', () => {
-            // Empty array added
-            basketItems = [];
-            store.dispatch(setBasketItems(basketItems));
+            newBasketItems = getEmpty();
+            store.dispatch(setBasketItems(newBasketItems));
             state = store.getState().basket;
             expect(state.basketItems).toStrictEqual([]);
         });
 
         test('should return an array with one item', () => {
-            basketItems = [{
-                uniqueCode: "0000",
-                name: "test",
-                count: 1
-            }];
-            store.dispatch(setBasketItems(basketItems));
+            newBasketItems = getOne();
+            store.dispatch(setBasketItems(newBasketItems));
             state = store.getState().basket;
             expect(state.basketItems).toHaveLength(1);
-            expect(state.basketItems).toStrictEqual(basketItems);
+            expect(state.basketItems).toStrictEqual(newBasketItems);
+        });
+
+        test('should return an array with two items', () => {
+            newBasketItems = getTwo();
+            store.dispatch(setBasketItems(newBasketItems));
+            state = store.getState().basket;
+            expect(state.basketItems).toHaveLength(2);
+            expect(state.basketItems).toStrictEqual(newBasketItems);
         });
 
     });
 
-    describe('test addBasketItems action', () => {          
+    describe('test addBasketItems action', () => {       
+        let newBasketItem: BasketItem = getFirst();
+
         test('should return an array with one item', () => {
-            let newBasketItem: BasketItem = {
-                uniqueCode: "0000",
-                name: "test",
-                count: 1
-            };
             store.dispatch(addBasketItems({ 
                 basketItem: newBasketItem
             }));
@@ -63,7 +99,50 @@ describe('basketSlice tests', () => {
             expect(state.basketItems).toHaveLength(1);
             expect(state.basketItems).toEqual([newBasketItem]);
         });
+    });
 
+    describe('test removeBasketItem action', () => {        
+        let newBasketItems: Array<BasketItem> = [];
+
+        test('should return an array with one item', () => {
+            // Setup
+            newBasketItems = getTwo();
+            store.dispatch(setBasketItems(newBasketItems));
+
+            store.dispatch(removeBasketItem({ 
+                index: 0 
+            }));
+            state = store.getState().basket;
+            expect(state.basketItems).toHaveLength(1);
+        });
+
+        test('should return empty array, dispatch ineffectual on empty intial array', () => {
+            store.dispatch(removeBasketItem({ 
+                index: 0 
+            }));
+            state = store.getState().basket;
+            expect(state.basketItems).toHaveLength(0);
+        });
+    });
+
+    describe('test updateBasketItem action', () => {  
+
+        test('should return item changed', () => {
+            // Setup
+            let newBasketItems: Array<BasketItem> = getOne();
+            store.dispatch(setBasketItems(newBasketItems));
+            
+            let newBasketItem: BasketItem = getSecond();
+            store.dispatch(updateBasketItem({
+                basketItem: newBasketItem,
+                index: 0
+            }));
+            state = store.getState().basket;
+            expect(state.basketItems).toHaveLength(1);
+            let changed = state.basketItems[0];
+            expect(changed?.uniqueCode).toBe(newBasketItem.uniqueCode);
+            expect(changed?.name).toBe(newBasketItem.name);
+        });
     });
     
 });
